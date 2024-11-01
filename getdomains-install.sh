4965d87290012d43cc5d599665f82409c6099174
Ldaborc <79675063+Ldaborc@users.noreply.github.com>
2024-11-01 20:39:53 +0800

    Update getdomains-install.sh

--- getdomains-install.sh
+++ getdomains-install.sh
@@ -783,8 +783,6 @@ EOF
         uci set network.@amneziawg_awg0[0].allowed_ips='0.0.0.0/0'
         uci set network.@amneziawg_awg0[0].endpoint_port=$AWG_ENDPOINT_PORT
         uci commit
-    fi
-
 }
 
 route_vpn () {
